79db6d1e5cde5cd2fe1cd8d45eb49f016326f8ab
lepton <tonihirvonen00@gmail.com>
2025-10-11 16:54:52 +0300

    feat: styling

--- lib/asconvws_web/components/file_input.ex
+++ lib/asconvws_web/components/file_input.ex
@@ -42,7 +42,7 @@ defmodule AsconvwsWeb.Layouts.FileInput do
 
   def input_form(assigns) do
     ~H"""
-    <div class="p-2 w-150">
+    <div class="p-2 max-w-150">
       <.mode_toggle mode={@mode} />
       
     <!-- Form -->
@@ -118,7 +118,7 @@ defmodule AsconvwsWeb.Layouts.FileInput do
         <.button
           type="submit"
           data-ripple-light="true"
-          class="mt-2 bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br transition-all shadow-md disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none px-4 py-2 rounded cursor-pointer"
+          variant="primary"
         >
           Submit
         </.button>
@@ -135,7 +135,7 @@ defmodule AsconvwsWeb.Layouts.FileInput do
     ~H"""
     <div class="mt-6">
       <div class="flex justify-between items-center">
-        <h2 class="font-bold mb-2">ASCII: {@filename}</h2>
+        <h2 class="font-bold mb-2">Input: {@filename}</h2>
 
         <div class="flex justify-between items-center space-x-10 mb-2">
           <label class="inline-flex items-center cursor-pointer space-x-2">
@@ -148,10 +148,8 @@ defmodule AsconvwsWeb.Layouts.FileInput do
               name="fit"
               checked={@fit}
               phx-click="toggle_fit"
-              class="sr-only peer"
+              class="toggle toggle-primary"
             />
-            <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600">
-            </div>
           </label>
 
           <.button
@@ -161,7 +159,7 @@ defmodule AsconvwsWeb.Layouts.FileInput do
             data-to="ascii-content"
             id="copy-clipboard"
             data-ripple-light="true"
-            class="mt-2 bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br transition-all shadow-md disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none px-4 py-2 rounded cursor-pointer"
+            variant="primary"
           >
             Copy to Clipboard
           </.button>
@@ -171,7 +169,7 @@ defmodule AsconvwsWeb.Layouts.FileInput do
         id="ascii-content"
         phx-hook="FitAscii"
         data-fit={if @fit, do: "true", else: "false"}
-        class="bg-black font-mono p-4 overflow-auto text-sm"
+        class="bg-black font-mono p-4 overflow-auto text-sm text-white-50"
       >{@ascii}</pre>
     </div>
     """
