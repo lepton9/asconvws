0ab4b9d093792ce288c185348bbaa2dd6dc86091
lepton <tonihirvonen00@gmail.com>
2025-10-11 14:49:42 +0300

    better fit and copy buttons

--- lib/asconvws_web/components/file_input.ex
+++ lib/asconvws_web/components/file_input.ex
@@ -137,26 +137,34 @@ defmodule AsconvwsWeb.Layouts.FileInput do
       <div class="flex justify-between items-center">
         <h2 class="font-bold mb-2">ASCII: {@filename}</h2>
 
-        <div class="flex justify-between items-center space-x-10">
-          <.input
-            type="checkbox"
-            label="Fit"
-            name="fit"
-            checked={@fit}
-            phx-click="toggle_fit"
-            class="m-0 checkbox checkbox-sm"
-          />
+        <div class="flex justify-between items-center space-x-10 mb-2">
+          <label class="inline-flex items-center cursor-pointer space-x-2">
+            <span class="ms-3 text-sm font-medium">
+              Fit
+            </span>
+            <.input
+              type="checkbox_bare"
+              label="Fit"
+              name="fit"
+              checked={@fit}
+              phx-click="toggle_fit"
+              class="sr-only peer"
+            />
+            <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600">
+            </div>
+          </label>
 
-          <button
-            class="mt-2 bg-blue-600 text-white px-4 py-2 rounded"
+          <.button
             phx-hook="CopyToClipboard"
             phx-update="ignore"
             phx-click="copy_to_clipboard"
             data-to="ascii-content"
             id="copy-clipboard"
+            data-ripple-light="true"
+            class="mt-2 bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br transition-all shadow-md disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none px-4 py-2 rounded cursor-pointer"
           >
             Copy to Clipboard
-          </button>
+          </.button>
         </div>
       </div>
       <pre
